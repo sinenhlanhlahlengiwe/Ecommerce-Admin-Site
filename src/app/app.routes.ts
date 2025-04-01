0842e3a5f230cd1863acb8e8e8005dfc166fc07f
Sinenhlanhla Shezi <sishezijhb024@student.wethinkcode.co.za>
2025-04-01 08:16:45 +0200

    removed typo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
- aimport { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
